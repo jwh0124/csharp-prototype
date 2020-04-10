079f794297b92ea6ed3bd2d36a8eda2764833d6f
whjung <whjung@cubox.aero>
2020-04-10 18:01:05 +0900

    + add RestApiExample

---
+++ RestApiExample/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestApiExample.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
