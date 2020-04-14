c5b4d7ca5fb300699a1396f61787c35a9f61c34a
whjung <whjung@cubox.aero>
2020-04-14 18:08:02 +0900

    + add worker service

---
+++ ServicePrototype/PrototypeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServicePrototype.Models;
+
+namespace ServicePrototype
+{
+    public class PrototypeContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=text.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ServicePrototype/Worker.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServicePrototype
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient httpClient;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            httpClient = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            httpClient.Dispose();
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var result = await httpClient.GetAsync("https://www.google.com");
+                
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Worker running at: {time}, Status Code {StatusCode}", DateTimeOffset.Now, result.StatusCode );
+                }
+                else
+                {
+                    _logger.LogError("{StatusCode}", result.StatusCode);
+                }
+
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
