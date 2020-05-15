7441596e2fce8a7eb2ff2a41e43eeb3f39708236
whjung <whjung@cubox.aero>
2020-05-15 18:06:13 +0900

    + add controller testing

--- TestCUPrototype/Controller/Config/TestControllerProvider.cs
+++ TestCUPrototype/Controller/Config/TestControllerProvider.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework.Internal;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -9,15 +12,14 @@ using System.Text;
 
 namespace TestCUPrototype.Config
 {
-    public class TestControllerProvider :IDisposable
+    public class TestControllerProvider : IDisposable
     {
         public TestServer testServer;
         public HttpClient Client { get; private set; }
         public TestControllerProvider()
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("E:\\CUBOX\\Private\\CSharp\\CUPrototype\\appsettings.json").Build();
-
-            testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>().UseConfiguration(configuration));
+            
+            testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
             
             Client = testServer.CreateClient();
         }
--- TestCUPrototype/Controller/User.cs
+++ TestCUPrototype/Controller/User.cs
@@ -1,11 +1,5 @@
-﻿using CUPrototype.Controller;
-using CUPrototype.DTO;
-using CUPrototype.Service;
-using CUPrototype.Service.Impl;
-using Microsoft.AspNetCore.Mvc;
+﻿using CUPrototype.DTO;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -26,6 +20,14 @@ namespace TestCUPrototype.Controller
         }
 
         [Fact]
+        public void GetUser()
+        {
+            var response = Client.GetAsync("/api/user/1");
+
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode);
+        }
+
+        [Fact]
         public void PostUser()
         {
             var response = Client.PostAsync("/api/user", new StringContent(JsonConvert.SerializeObject(new UserDto()
@@ -34,6 +36,25 @@ namespace TestCUPrototype.Controller
             }),Encoding.UTF8, "application/json"));
 
             Assert.Equal(HttpStatusCode.Created, response.Result.StatusCode);
+        }
+
+        [Fact]
+        public void PutUser()
+        {
+            var response = Client.PutAsync("/api/user/1", new StringContent(JsonConvert.SerializeObject(new UserDto()
+            {
+                Name = "Kim"
+            }), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.NoContent, response.Result.StatusCode);
+        }
+
+        [Fact]
+        public void DeleteUser()
+        {
+            var response = Client.DeleteAsync("/api/user/1");
+
+            Assert.Equal(HttpStatusCode.NoContent, response.Result.StatusCode);
         }
     }
 }
