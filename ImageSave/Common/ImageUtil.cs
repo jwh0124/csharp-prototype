93978e8cfd7d96623a6a3f178a8ba81ba6018876
whjung <whjung@cubox.aero>
2020-05-29 13:29:38 +0900

    + add base64 image save prototype

---
+++ ImageSave/Common/ImageUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ImageSave.Common
+{
+    public class ImageUtil
+    {
+        public string ImageConvertToBase64(string Path)
+        {
+            return Convert.ToBase64String(File.ReadAllBytes(Path));
+        }
+
+        public void Base64ConvertToImage(string Base64, string writePath, string ImageName)
+        {
+            byte[] imageBytes = Convert.FromBase64String(Base64);
+
+            try {
+                File.WriteAllBytes(writePath + "\\" + ImageName, imageBytes);
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+        }
+    }
+}
---
+++ ImageSave/Program.cs
@@ -0,0 +1,19 @@
+﻿using ImageSave.Common;
+using System;
+
+namespace ImageSave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:\\CUBOX\\DOC\\업무\\MVC Pattern.png";
+            string writePath = "C:\\test";
+
+            ImageUtil util = new ImageUtil();
+            string base64 = util.ImageConvertToBase64(path);
+
+            util.Base64ConvertToImage(base64, writePath, Guid.NewGuid().ToString() + ".jpg");
+        }
+    }
+}
