1dfe35c7d50ddedbff139fa0ef439baed0b3c205
whjung <jwh0124@gmail.com>
2020-06-15 22:12:26 +0900

    add Packet Test Console

---
+++ PacketTestConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PacketTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                string readPacket = Console.ReadLine();
+                var resultPacket = string.Empty;
+                if (readPacket.Equals("02"))
+                {
+                    resultPacket += readPacket;
+
+                    Console.ReadLine();
+
+                    int dataLength = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < dataLength; i++)
+                    {
+                        resultPacket += Console.ReadLine();
+                    }
+
+                    Console.WriteLine(resultPacket);
+                }
+            }            
+
+        }
+    }
+}
