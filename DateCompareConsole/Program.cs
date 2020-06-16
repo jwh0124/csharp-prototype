a24d8a13d6099c6d440dcbe1a6f791b359ad6c55
whjung <whjung@cubox.aero>
2020-06-16 17:16:28 +0900

    add date compare console prototype

---
+++ DateCompareConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateCompareConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDt = DateTime.Parse("2020-06-17T07:30:44.577");
+
+            DateTime endDt = DateTime.Parse("2020-06-16T07:30:44.577");
+
+            DateTime currentDate = DateTime.Now;
+
+            Console.WriteLine(DateTime.Compare(currentDate, startDt) >= 0);
+            Console.WriteLine(DateTime.Compare(currentDate, endDt) <= 0);
+            Console.WriteLine(!(DateTime.Compare(currentDate, startDt) >= 0 && DateTime.Compare(currentDate, endDt) <= 0));
+        }
+    }
+}
