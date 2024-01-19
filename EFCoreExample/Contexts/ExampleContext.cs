26c5cd374445fc90e70a577fca0b83d0bf7ed278
jwh0124 <jwh0124@gmail.com>
2024-01-19 09:24:58 +0900

    EFCore Example initialized

---
+++ EFCoreExample/Contexts/ExampleContext.cs
@@ -0,0 +1,25 @@
+﻿using EFCoreExample.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreExample.context
+{
+    public class ExampleContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public string DbPath { get; }
+
+        public ExampleContext() {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "example.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"DataSource={DbPath}");
+    }
+}
---
+++ EFCoreExample/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCoreExample.context;
+using EFCoreExample.Models;
+
+using var db = new ExampleContext();
+
+Console.WriteLine($"Database path : {db.DbPath}");
+
+// Delete
+Console.WriteLine("Clean up database");
+db.Users.RemoveRange(db.Users);
+
+// Create
+Console.WriteLine("Inserting a new user");
+db.Add(new User { Id = 1, Name = "tester" });
+db.SaveChanges();
+
+
+// Read
+Console.WriteLine("Querying for a user");
+var user = db.Users.First();
+Console.WriteLine(user.Id);
+Console.WriteLine(user.Name);
+
+// Update
+Console.WriteLine("Updating the user");
+user.Name = "Test";
+db.SaveChanges();
