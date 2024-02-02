37b129dea3e4e4371706a5f3a213ce45eacaceb4
jwh0124 <jwh0124@gmail.com>
2024-02-02 18:22:52 +0900

    Marshal example source update

---
+++ MarshalExample/CParam.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MarshalExample
+{
+	public class CParam
+	{
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string? firstName;
+
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string? lastName;
+    }
+}
+
---
+++ MarshalExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using MarshalExample;
+
+class Program
+{
+    const string dllName = "Example.dll";
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int Sum(int firstValue, int secondValue);
+
+    [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern string FullName(nint pParam);
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Start Marshalling");
+
+        int sumResult = Sum(1, 2);
+
+        Console.WriteLine("1 + 2 = {sumResult}", sumResult);
+
+        CParam cParam = new() { firstName="Circle", lastName="Jung" };
+
+        IntPtr cParamPtr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(CParam)));
+
+        Marshal.StructureToPtr(cParam, cParamPtr, false);
+
+        string fullName = FullName(cParamPtr);
+
+        Marshal.FreeHGlobal(cParamPtr);
+
+        Console.WriteLine("Full name : {fullName}", fullName);
+
+        Console.WriteLine("Stop Marshalling");
+    }
+}
