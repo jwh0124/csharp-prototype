ae074b79d147e051c7388c04d0bc15536516026b
jwh0124 <jwh0124@gmail.com>
2024-03-12 14:02:43 +0900

    MultiProject prototype

---
+++ MultiProject/ClassLibrary/WorkerService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ClassLibrary
+{
+    public class WorkerService : BackgroundService
+    {
+        private readonly ILogger<WorkerService> _logger;
+
+        public WorkerService(ILogger<WorkerService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
