a09e66be711147d84fc1a8e3dc8e92ed9a576188
jwh0124 <jwh0124@gmail.com>
2024-03-12 14:02:58 +0900

    TypeComparePrototype

---
+++ TypeComparePrototype/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SHVXGate.ServiceContracts;
+using TypeComparePrototype;
+
+SHVXGate.ServiceContracts.ReturnData returnData = new SHVXGate.ServiceContracts.ReturnData();
+
+TypeComparePrototype.ReturnData returnData1 = new TypeComparePrototype.ReturnData();
+
+Console.WriteLine(returnData.GetType());
+Console.WriteLine(returnData1.GetType());
+Console.WriteLine(typeof(SHVXGate.ServiceContracts.ReturnData).IsInstanceOfType(returnData));
+
---
+++ TypeComparePrototype/ReturnData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeComparePrototype
+{
+    [Serializable]
+    public class ReturnData
+    {
+        private string site = null;
+
+        private string name = null;
+
+        private string status = null;
+
+        private DateTime nowdate = default(DateTime);
+
+        private string commondata1 = null;
+
+        private string commondata2 = null;
+
+        private string commondata3 = null;
+
+        private string commondata4 = null;
+
+        private string commondata5 = null;
+
+        private string rev1 = null;
+
+        private string rev2 = null;
+
+        private string rev3 = null;
+
+        private string rev4 = null;
+
+        private string rev5 = null;
+
+        private string msgID = null;
+
+        private List<int> iid = new List<int>();
+    }
+}
