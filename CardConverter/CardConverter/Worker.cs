e7e0b70eeacc974a4ba46b0ada048a501adbbef5
jwh0124 <jwh0124@gmail.com>
2024-05-21 10:50:20 +0900

    card convert test

--- CardConverter/CardConverter/Worker.cs
+++ CardConverter/CardConverter/Worker.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.Logging;
 using SHSystem.BS2.CardConverter;
 using System.Text;
 
@@ -17,6 +16,54 @@ namespace CardConverter
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            byte[] xStationNewFirmwareTest = [ 
+                51, 55, 51, 51, 51, 54, 51,
+                56, 51, 57, 51, 57, 51, 48, 
+                51, 56, 0, 0, 0, 0, 0, 
+                0, 0, 0, 0, 0, 0, 0, 
+                0, 0, 0, 0 ];
+
+             byte[] xStationNewScanDataTest = [
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x55, 0x51, 0x54, 0x56,
+                0x57, 0x57, 0x48, 0x56 ];
+
+            byte[] hexByteList = [ 
+                0x33, 0x37, 0x33, 0x33, 0x33, 0x36, 0x33,
+                0x38, 0x33, 0x39, 0x33, 0x39, 0x33, 0x30, 
+                0x33, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00 ];
+
+            byte[] hexScanByteList = [
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x37, 0x33, 0x36, 0x38,
+                0x39, 0x39, 0x30, 0x38 ];
+
+            byte[] xStationNewFirmwareTest2 = [ 
+                0x52, 0x52, 0x52, 0x50, 0x51, 0x54, 0x51,
+                0x55, 0x52, 0x52, 0x52, 0x54, 0x51, 0x54, 
+                0x52, 0x49, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00 ];
+
+            byte[] hexByteList2 = [ 
+                0x34, 0x34, 0x34, 0x32, 0x33, 0x36, 0x33,
+                0x37, 0x34, 0x34, 0x34, 0x36, 0x33, 0x36, 
+                0x34, 0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00 ];
+
+            var hexStringtest = Convert.ToBase64String(xStationNewFirmwareTest);
+
+            _logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2MSG", hexStringtest));
+
+            var test = Convert.FromBase64String(_cardConverter.CardConverter("SH.GOV.64.BS2V2", "0000000073689908"));
+
             byte[] xStationNewFirmware = [ 
                 0x51, 0x55, 0x51, 0x51, 0x51, 0x54, 0x51,
                 0x56, 0x51, 0x57, 0x51, 0x57, 0x51, 0x48, 
@@ -24,29 +71,12 @@ namespace CardConverter
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
                 0x00, 0x00, 0x00, 0x00 ];
 
-            var xStationNewUserId = 3733363839393038;
-
-            var xStationNewBase64 = Convert.ToBase64String(xStationNewFirmware);
-
-            _logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2MSG", xStationNewBase64));
-
-            _logger.LogInformation(xStationNewBase64);
-
             byte[] xStationOldFirmware = [
                 0x51, 0x52, 0x53, 0x55, 0x53, 0x56, 0x55,
                 0x52, 0x55, 0x50, 0x00, 0x00, 0x00, 0x00, 
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00,
-
-            ];
-            var xStationOldUserId = 3457587472;
-
-            var xStationOldBase64 = Convert.ToBase64String(xStationOldFirmware);
-
-            _logger.LogInformation(xStationOldBase64);
-
-            _logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2MSG", xStationOldBase64));
+                0x00, 0x00, 0x00, 0x00];
 
             byte[] xPassStandardFirmware = [
                 0x51, 0x52, 0x53, 0x55, 0x53, 0x56, 0x55,
@@ -55,38 +85,88 @@ namespace CardConverter
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
                 0x00, 0x00, 0x00, 0x00 ];
 
-            var xPassUserId = 3457587472;
+            byte[] xPassOldFirmware = [
+                0x51, 0x55, 0x51, 0x51, 0x51, 0x54, 0x51,
+                0x56, 0x51, 0x57, 0x51, 0x57, 0x51, 0x48, 
+                0x51, 0x56, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00 ];
 
-            var xPassBase64 = Convert.ToBase64String(xPassStandardFirmware);
+            byte[] xStationNewScanData = [
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x55, 0x51, 0x54, 0x56,
+                0x57, 0x57, 0x48, 0x56 ];
 
-            _logger.LogInformation(xPassBase64);
+            string xStationNewScanDataHex = "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-37-33-36-38-39-39-30-38";
 
-            _logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2MSG", xPassBase64));
+            byte[] xStationOldScanData = [
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0xCE, 0x16, 0x99, 0x10 ];
 
+            string xStationOldScanDataHex = "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-CE-16-99-10";
+            
 
+            byte[] xPassStandardScanData = [
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0xCE, 0x16, 0x99, 0x10 ];
 
+            byte[] xPassOldScanData = [
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x55, 0x51, 0x54, 0x56,
+                0x57, 0x57, 0x48, 0x56 ];
+
+            string xPassOldScanDataHex = "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-37-33-36-38-39-39-30-38";
+
+            #region Test
+            //_logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2MSG", Convert.ToBase64String(xStationNewFirmware)));
+
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITYMSG", xStationNewBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITYMSG", xStationOldBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITYMSG", xPassBase64));
+
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITYMSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITYMSG", xStationOldScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITYMSG", xPassScanBase64));
+
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITYMSG", xStationNewBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITYMSG", xStationOldBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITYMSG", xPassBase64));
+
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITYMSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITYMSG", xStationOldScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITYMSG", xPassScanBase64));
 
 
             //_logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2MSG", Convert.ToBase64String(expected)));
 
             //_logger.LogInformation(Convert.ToBase64String(expected));
 
-            //_logger.LogInformation(_cardConverter.CardConverter("SHBTYPE34",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("SH.HID.STD.26",test));
-            ////_logger.LogInformation(_cardConverter.CardConverter("MIFARE32",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("SH.CTD.68",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("SH.HID.CTD.37",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("ICLASS26",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("SHTYPE35",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("SHTYPE80",test));
-            ////_logger.LogInformation(_cardConverter.CardConverter("MIFARE34",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE32BS2",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHPARITY",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITY",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHPARITY",test));
-            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITY",test));
+            //_logger.LogInformation(_cardConverter.CardConverter("SHBTYPE34MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("SH.HID.STD.26MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE32MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("SH.CTD.68MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("SH.HID.CTD.37MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("ICLASS26MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("SHTYPE35MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("SHTYPE80MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE34MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE32BS2MSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHPARITYMSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-STANDARD-WITHOUTPARITYMSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHPARITYMSG", xStationNewScanBase64));
+            //_logger.LogInformation(_cardConverter.CardConverter("MIFARE-REVERSE-WITHOUTPARITYMSG", xStationNewScanBase64));
 
             //byte[] expected = {
             //    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -95,6 +175,7 @@ namespace CardConverter
             //    0x35, 0x38, 0x41, 0x38, 0x39, 0x45, 0x46, 0x41 };
 
             //var cardNo = "0000000058A89EFA";
+            
 
             //_logger.LogInformation(_cardConverter.CardConverter("SH.GOV.64.BS2V2", cardNo));
 
@@ -116,6 +197,16 @@ namespace CardConverter
 
             //var bytes = Convert.FromBase64String("MzEzNzM0MzAzMTMwMzgzMDM3MzcAAAAAAAAAAAAAAAA=");
             //_logger.LogInformation("{CSHGov64BS2V2MSG}", CSHGov64BS2V2MSG(Convert.ToBase64String(bytes)));
+
+            #endregion
+        }
+
+
+        public byte[] ByteArrayToHexByteArray(byte[] bytes)
+        {
+            bytes.Where(b => b.Equals(0x00)).ToList().ForEach(b => b = Convert.ToByte(Convert.ToString(b, 16)));
+
+            return bytes;
         }
 
         public string CSHGov64BS2V2(string Data)
---
+++ testConvert/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+byte[] xStationNewByte = [
+    51, 49, 51, 55, 51, 52, 51, 49, 51, 48, 
+    51, 52, 51, 57, 51, 57, 51, 48, 51, 51,
+    51, 56, 51, 51, 51, 54,  0,  0,  0,  0, 
+    0, 0 ];
+
+string testCode = string.Empty;
+
+xStationNewByte.ToList().ForEach(a => testCode += (Convert.ToChar(a)));
+
+Console.WriteLine(testCode);
+
+Console.ReadLine();
